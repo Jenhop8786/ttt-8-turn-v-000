970c336b531e9ee9f9c1ebb01f4a977e1e80577d
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-11-13 15:06:09 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,38 @@
+
+board = ["X", " ", " ", " ", " ", " ", " ", " ", "X"]
+
+def display_board(board)
+ puts "display_board(board)"
+ puts " #{board[0]} | #{board[8]} | #{board[5]}  "
+ puts "-----------"
+ puts " #{board[1]} | #{board[2]} | #{board[3]}  "
+ puts "-----------"
+ puts " #{board[7]} | #{board[6]} | #{board[4]}  "
+
+end
+
+def move(board, position, character = " ")
+   board[position] = character
+end
+
+def valid_move?(board, index)
+  !position_taken?(board, index) && index.between?(0, 8)
+end
+board = ["X", "X", "X", "X", "O", "O", " X", "O", "X"]
+def turn(board)
+  puts " #{board[0]} | #{board[8]} | #{board[5]}  "
+  puts "-----------"
+  puts " #{board[1]} | #{board[2]} | #{board[3]}  "
+  puts "-----------"
+  puts " #{board[7]} | #{board[6]} | #{board[4]}  "
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+end
+
+def position_taken?(board, index)
+  !(board[index] == " " || board[index] == "" || board[index] == nil)
+end
+
+def input_to_index(position)
+  position.to_i - 1
+end
